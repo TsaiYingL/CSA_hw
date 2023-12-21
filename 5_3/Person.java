affe196028358c5eb1a1a4fb1458cc5d8eb31d3c
TsaiYing <tsaiying11100815@gmail.com>
2023-12-20 23:10:46 -0500

    completed 5_3

---
+++ 5_3/Person.java
@@ -0,0 +1,28 @@
+/**
+ * This is a class that represents a Person.
+ */
+public class Person {
+
+    private String name;
+
+    //Constructor to create a Person object
+    public Person(String name) {
+        this.name = name;
+    }
+
+    //Getter method to retrieve the name of the person.
+    public String getName() {
+        return name;
+    }
+
+    //Set the name of the person
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //A simple greeting
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name + "!");
+    }
+
+}
